83d70ec2d6986f47f968bfec394ec788afa34a28
Artem <artembl487@gmail.com>
2023-03-04 04:57:41 +0300

    added popup/task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,11 @@
+const arrPopup = [...document.querySelectorAll(".modal")];
+const close = [...document.querySelectorAll(".modal__close_times")];
+const showSuccess = document.querySelector(".show-success");
+
+arrPopup.forEach(item => {
+    if(item.id === "modal_main")item.classList.add('modal_active');
+    showSuccess.onclick = () => item.classList.add('modal_active');
+});
+
+close.forEach(item => item.onclick = () => arrPopup.forEach(item => item.className = 'modal'));
+
