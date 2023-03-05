6dbf35055c5c69f8a834098304daaa045d812af8
Artem <artembl487@gmail.com>
2023-03-06 00:39:16 +0300

    added fixes element-search/popup, slider .js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,11 +2,18 @@ const arrPopup = [...document.querySelectorAll(".modal")];
 const close = [...document.querySelectorAll(".modal__close_times")];
 const showSuccess = document.querySelector(".show-success");
 
-arrPopup.forEach(item => {
-    if(item.id === "modal_main")item.classList.add('modal_active');
-    showSuccess.onclick = () => item.classList.add('modal_active');
-});
 
-close.forEach(item => item.onclick = () => arrPopup.forEach(item => item.className = 'modal'));
+function togglePopupMain(){
+    arrPopup[0].classList.toggle('modal_active');
+}
+function closePopup(){
+    arrPopup.forEach((element) => element.classList.remove('modal_active'));
+}
+togglePopupMain();
 
+showSuccess.onclick = () => {
+    togglePopupMain();
+    arrPopup[1].classList.add('modal_active');
+}
 
+close.forEach((element) => element.onclick = closePopup);
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,30 +1,29 @@
 const arrSlide = [...document.querySelectorAll(".slider__item")];
 const prewArrow = document.querySelector(".slider__arrow_prev");
 const nextArrow = document.querySelector(".slider__arrow_next");
-let index = 0;
 
-prewArrow.onclick = () => {
-    if(index === 0){
-        index = arrSlide.length - 1;
-        arrSlide[index].classList.add("slider__item_active");
-        arrSlide[0].classList.remove("slider__item_active");
-        return;
-    }
-    arrSlide[index].classList.remove("slider__item_active");
-    index -= 1;
-    arrSlide[index].classList.add("slider__item_active");
-};
+function nextElement(i, length){
+    if (i === length - 1){return 0};
+    return i += 1;
+}
+function prewElement(i, length){
+    if (i === 0) return length - 1;
+    return i -= 1;
+}
 
-nextArrow.onclick = () => {
-    if(index === arrSlide.length - 1){
-        index = 0;
-        arrSlide[index].classList.add("slider__item_active");
-        arrSlide[arrSlide.length - 1].classList.remove("slider__item_active");
-        return;
-    }
-    index += 1;
-    arrSlide[index].classList.add("slider__item_active");
-    arrSlide[index - 1].classList.remove("slider__item_active");
-};
+nextArrow.onclick = () =>{
+    let indexActiveSlide = arrSlide.findIndex(item => item.classList.contains("slider__item_active"));
+    let indexNextSlide = nextElement(indexActiveSlide, arrSlide.length);
+    arrSlide[indexActiveSlide].classList.remove("slider__item_active");
+    arrSlide[indexNextSlide].classList.add("slider__item_active");
+}
+prewArrow.onclick = () =>{
+    let indexActiveSlide = arrSlide.findIndex(item => item.classList.contains("slider__item_active"));
+    let indexPrewSlide = prewElement(indexActiveSlide, arrSlide.length);
+    arrSlide[indexActiveSlide].classList.remove("slider__item_active");
+    arrSlide[indexPrewSlide].classList.add("slider__item_active");
+}
+
+
 
 
